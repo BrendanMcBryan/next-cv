8397bbf78e8ee333c2577a017fe75dab61e70a46
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-01-23 09:26:54 -0800

    db skelton start

---
+++ app/lib/starterdata.js
@@ -0,0 +1,24 @@
+// this file =contains intial seeding data to practice db set up
+
+const projects = [
+  {
+    id: "0001",
+    title: "",
+    subtitle: "",
+    short_description: "",
+    long_description: "",
+    category: "",
+    tech: [],
+    skills: [],
+  },
+  {
+    id: "0002",
+    title: "",
+    subtitle: "",
+    short_description: "",
+    long_description: "",
+    category: "",
+    tech: [],
+    skills: [],
+  },
+];
