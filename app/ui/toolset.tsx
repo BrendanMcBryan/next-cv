b18da0eff41e002b08a6d31ea1102d08df7d2f3a
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-01-22 16:11:24 -0800

    added and adjusted some tools

--- app/ui/toolset.tsx
+++ app/ui/toolset.tsx
@@ -7,7 +7,7 @@ import {
   SiWordpress,
   SiAdobe,
   SiAdobeacrobatreader,
-  SiTypescript, SiAdobexd, SiWix
+  SiTypescript, SiAdobexd, SiWix, SiTailwindcss, SiNextdotjs
 } from "react-icons/si";
 import { PiCodeSimpleBold } from "react-icons/pi";
 import { TbBrandVscode } from "react-icons/tb";
@@ -16,31 +16,37 @@ import { TbWorldCode } from "react-icons/tb";
 
 
 export default function ToolSet() {
-  const icon_class = "text-2xl me-1 text-pink-600"
-  const group_class = "text-3xl text-cyan-900 me-3"
+  const icon_class = "text-xl me-1 text-pink-600"
+
+  const group_class = "text-3xl text-cyan-900 me-4"
+
   return <div className="flex flex-col mt-4">
-    <div id="designtools" className="flex flex-row items-center">
+    <div id="designtools" className="flex flex-row items-center mb-3">
       <SiAdobe className={group_class} />
+
       <SiAdobeindesign className={icon_class} />
       <SiAdobeillustrator className={icon_class} />
       <SiAdobephotoshop className={icon_class} />
       <SiAdobexd className={icon_class} />
       <SiAdobeacrobatreader className={icon_class} />
-
     </div>
-    <div id="webtools" className="flex flex-row">
+
+    <div id="developertools" className="flex flex-row items-center mb-3">
+      <PiCodeSimpleBold className={group_class} />
+
+      <SiJavascript className={icon_class} />
+      <SiTypescript className={icon_class} />
+      <SiTailwindcss className={icon_class} />
+      <SiNextdotjs className={icon_class} />
+    </div>
+
+    <div id="webtools" className="flex flex-row items-center mb-3">
       <TbWorldCode className={group_class} />
 
       <SiWordpress className={icon_class} />
       <SiSquarespace className={icon_class} />
       <SiWix className={icon_class} />
     </div>
-    <div id="developertools" className="flex flex-row">
-      <TbWorldCode className={group_class} />
-      <SiJavascript className={icon_class} />
-      <SiTypescript className={icon_class} />
-      <SiWordpress className={icon_class} />
-      <SiSquarespace className={icon_class} />
-    </div>
+
   </div>
 }
