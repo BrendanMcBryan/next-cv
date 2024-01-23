2c556b2643b1d2ed8459fe303acb286f99fcce3a
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-01-23 15:14:12 -0800

    adjuste project from local data

--- app/ui/project.tsx
+++ app/ui/project.tsx
@@ -1,11 +1,26 @@
-import Image from "next/image";
-import di_logo from "@/public/DesigningIntentLogo.webp"
-import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode } from "react";
-export default function Project(props: { title: string }) {
-  return (
-    <div className="project flex p-24 justify-start w-screen">
-      <h1 className="text-2xl">{props.title}</h1>
-    </div>
 
-  );
+import Image from "next/image";
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, PromiseLikeOfReactNode, Key } from "react";
+const { projects } = require("@/app/lib/starterdata");
+// import { fetchProjects } from '@/app/lib/data'
+export default function Projects() {
+  // const projects = await fetchProjects();
+  // console.log(projects);
+  const renderProjects = projects.map((item: {
+    name: string; title: string;
+  }, index: Key) => {
+    return (
+      <div key={index} className="bg-slate-600 p-12 hover:bg-slate-400">
+        <h1 className="text-xl">{item.name}</h1>
+        <h2 className="text-sm">{item.title}</h2>
+
+      </div>
+    )
+
+  });
+  // console.log(renderProjects);
+
+  return (<div>{renderProjects}</div>)
+
+
 }
