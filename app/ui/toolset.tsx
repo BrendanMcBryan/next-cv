374443dc4f0f7f0e7a272805df79b68420a79294
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-01-22 16:34:03 -0800

    additional tool icons

--- app/ui/toolset.tsx
+++ app/ui/toolset.tsx
@@ -7,12 +7,13 @@ import {
   SiWordpress,
   SiAdobe,
   SiAdobeacrobatreader,
-  SiTypescript, SiAdobexd, SiWix, SiTailwindcss, SiNextdotjs
+  SiTypescript, SiAdobexd, SiWix, SiTailwindcss, SiNextdotjs, SiHtml5, SiCsswizardry, SiFigma, SiBootstrap, SiMicrosoftoffice, SiAsana, SiSlack, SiApple
 } from "react-icons/si";
-import { PiCodeSimpleBold } from "react-icons/pi";
+import { PiCodeSimpleBold, PiOfficeChair } from "react-icons/pi";
 import { TbBrandVscode } from "react-icons/tb";
 import { RiJavascriptFill } from "react-icons/ri";
 import { TbWorldCode } from "react-icons/tb";
+import { FaGoogleDrive } from "react-icons/fa";
 
 
 export default function ToolSet() {
@@ -37,7 +38,10 @@ export default function ToolSet() {
       <SiJavascript className={icon_class} />
       <SiTypescript className={icon_class} />
       <SiTailwindcss className={icon_class} />
+      <SiBootstrap className={icon_class} />
       <SiNextdotjs className={icon_class} />
+      <SiHtml5 className={icon_class} />
+      <SiCsswizardry className={icon_class} />
     </div>
 
     <div id="webtools" className="flex flex-row items-center mb-3">
@@ -46,7 +50,17 @@ export default function ToolSet() {
       <SiWordpress className={icon_class} />
       <SiSquarespace className={icon_class} />
       <SiWix className={icon_class} />
+      <SiFigma className={icon_class} />
     </div>
+    <div id="officeadmintools" className="flex flex-row items-center mb-3">
+      <PiOfficeChair className={group_class} />
 
+      <SiMicrosoftoffice className={icon_class} />
+      <FaGoogleDrive className={icon_class} />
+      <SiApple className={icon_class} />
+      <SiAsana className={icon_class} />
+      <SiSlack className={icon_class} />
+
+    </div>
   </div>
 }
