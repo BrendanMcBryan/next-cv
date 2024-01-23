90f5be81da181e7c9c22de38613a10d6afbcc05d
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-01-23 05:34:46 -0800

    more tools

--- app/ui/toolset.tsx
+++ app/ui/toolset.tsx
@@ -7,19 +7,21 @@ import {
   SiWordpress,
   SiAdobe,
   SiAdobeacrobatreader,
-  SiTypescript, SiAdobexd, SiWix, SiTailwindcss, SiNextdotjs, SiHtml5, SiCsswizardry, SiFigma, SiBootstrap, SiMicrosoftoffice, SiAsana, SiSlack, SiApple
+  SiTypescript, SiAdobexd, SiWix, SiTailwindcss, SiNextdotjs, SiHtml5, SiCsswizardry, SiFigma, SiBootstrap, SiMicrosoftoffice, SiAsana, SiSlack, SiApple, SiNodedotjs, SiGithub, SiReddit, SiGoodreads
 } from "react-icons/si";
 import { PiCodeSimpleBold, PiOfficeChair } from "react-icons/pi";
 import { TbBrandVscode } from "react-icons/tb";
 import { RiJavascriptFill } from "react-icons/ri";
 import { TbWorldCode } from "react-icons/tb";
 import { FaGoogleDrive } from "react-icons/fa";
+import Link from "next/link";
 
+import { IoShareSocialOutline } from "react-icons/io5";
 
 export default function ToolSet() {
-  const icon_class = "text-xl me-1 text-pink-600"
+  const icon_class = " me-1 text-pink-600"
 
-  const group_class = "text-3xl text-cyan-900 me-4"
+  const group_class = "text-3xl text-cyan-900 me-2"
 
   return <div className="flex flex-col mt-4">
     <div id="designtools" className="flex flex-row items-center mb-3">
@@ -34,7 +36,10 @@ export default function ToolSet() {
 
     <div id="developertools" className="flex flex-row items-center mb-3">
       <PiCodeSimpleBold className={group_class} />
-
+      <Link href="https://github.com/BrendanMcBryan" target="_blank" >
+        <SiGithub className={icon_class} />
+      </Link>
+      <SiNodedotjs className={icon_class} />
       <SiJavascript className={icon_class} />
       <SiTypescript className={icon_class} />
       <SiTailwindcss className={icon_class} />
@@ -60,6 +65,19 @@ export default function ToolSet() {
       <SiApple className={icon_class} />
       <SiAsana className={icon_class} />
       <SiSlack className={icon_class} />
+
+    </div>
+    <div id="socials" className="flex flex-row items-center mb-3">
+      <IoShareSocialOutline className={group_class} />
+      <Link href="https://www.reddit.com/user/designingintent">
+        <SiReddit className={icon_class} />
+      </Link>
+      <Link href="https://www.goodreads.com/user/show/4488078-brendan">
+        <SiGoodreads className={icon_class}/>
+      </Link>
+      <Link href="https://github.com/BrendanMcBryan" target="_blank" >
+        <SiGithub className={icon_class} />
+      </Link>
 
     </div>
   </div>
