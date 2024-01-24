05426723e50d592bf5892336eced867e07e14c10
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-01-24 10:51:12 -0800

    adjustments to sticky pdf download menu

--- app/ui/stickysidemenu.tsx
+++ app/ui/stickysidemenu.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 
@@ -9,11 +10,13 @@ import Image from "next/image";
 export default function StickMenu({ show, setter }: { show: any, setter: any }) {
 
   // Define our base class
-  const className = " font-light text-sm text-white ms-2 transition-[margin-right] ease-in-out duration-300 hover:font-bold cursor-pointer";
-  const appendClass = show ? "mr-0" : " mr-[-250px] md:ml-0";
+  const linkClass = " font-light text-sm text-white w-[90px] ms-2 transition-[margin-right] ease-in-out duration-300 hover:font-bold cursor-pointer";
+  const linkNoteClass = "text-xs text-white italic w-[90px] ms-2 transition-[margin-right] ease-in-out duration-300";
+  
+  const appendClass = show ? "mr-0" : " mr-[-102px] md:ml-0";
 
 
-  return (<div className='fixed inset-y-0 right-0 top-1/3 flex flex-col items-center justify-center'>
+  return (<div className='fixed inset-y-0 right-0 flex flex-col items-center justify-center'>
     <aside className="flex flex-col p-3 bg-pink-600 rounded-l-lg"
       onMouseEnter={() => {
         setter((oldVal: any) => !oldVal);
@@ -24,21 +27,24 @@ export default function StickMenu({ show, setter }: { show: any, setter: any }) 
 
     >
 
-      <div className="flex items-center">
-        <Image className="hover:animate-pulse " src={DILogo} alt="DI logo" width={20} height={20} />
-        {/* <h2 className={`${className}${appendClass}`}>downloads</h2> */}
-        
+
+      <div className="flex items-center justify-start">
+        <Image className="hover:animate-pulse flex flex-row me-3 max-w-[20px]" src={DILogo} alt="DI logo" width={20} height={20} layout="responsive" />
+
+        <h2 className={`${linkNoteClass}${appendClass}`}>pdf downloads</h2>
+
+
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center justify-start">
         <IoDocument className="text-white my-3 me-3 hover:animate-pulse text-xl" />
-        <Link href="/McBryan_resume.pdf" locale={false} target="_blank" className={`${className}${appendClass}`}>
+        <Link href="/McBryan_resume.pdf" locale={false} target="_blank" className={`${linkClass}${appendClass}`}>
           <h2>resume</h2>
         </Link>
-        
+
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center justify-start">
         <IoImages className="text-white me-3 hover:animate-pulse text-xl" />
-        <Link href="/McBryan_portfolio.pdf" locale={false} target="_blank" className={`${className}${appendClass}`}>
+        <Link href="/McBryan_portfolio.pdf" locale={false} target="_blank" className={`${linkClass}${appendClass}`}>
           <h2>portfolio</h2>
         </Link>
       </div>
