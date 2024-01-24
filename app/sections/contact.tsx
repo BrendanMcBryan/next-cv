9dee797f544ac77bb3df02323dc91e40c42ef4cd
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-01-23 16:46:00 -0800

    contact section adjusted sizing

--- app/sections/contact.tsx
+++ app/sections/contact.tsx
@@ -1,5 +1,22 @@
-export default function Contact() {
+import { MdCrop, MdOutlineCode, MdOutlineBuild } from "react-icons/md";
+
+
+export default function contactSection() {
+  
   return <>
-    <section className="flex w-screen bg-slate-600 p-24"><div className="text-3xl">contact</div></section>
+    <section
+      className="flex flex-col w-full items-start justify-start bg-slate-100 py-12 "
+    >
+      <div className="flex flex-col w-full mx-auto bg-slate-200 max-w-[1200px]">
+        <div className="p-12 w-full flex-1 flex flex-col justify-start">
+          <h2 className="text-2xl font-light mb-2">contact brendan</h2>
+          <p className="font-light text-slate-700">I contact me with the form below</p>
+        </div>
+        
+      </div>
+
+
+
+    </section>
   </>
 }
