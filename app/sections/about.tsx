4b14bcda0dbc3622859e289cb11f0fd25ddd5af4
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-01-24 08:01:57 -0800

    column and mobile adjustments

--- app/sections/about.tsx
+++ app/sections/about.tsx
@@ -2,20 +2,20 @@ import { MdCrop, MdOutlineCode, MdOutlineBuild } from "react-icons/md";
 
 
 export default function aboutSection() {
-  const cardClass = " card flex flex-col justify-between bg-cyan-50  w-1/3 rounded-lg shadow-lg me-12 py-8 px-4 hover:bg-cyan-100"
+  const cardClass = " card flex flex-col justify-between bg-cyan-50 rounded-lg shadow-lg mb-12 mx-0 py-8 px-4 hover:bg-cyan-100"
   const titleClass = "font-extralight text-center text-5xl text-slate-700 mb-4"
   const liClass = " text-slate-700"
-  const iconClass = " text-5xl text-center text-pink-300 mx-auto mb-3"
+  const iconClass = " text-5xl text-center text-white mx-auto mb-3 "
   return <>
     <section
       className="flex flex-col items-start justify-start  py-12 "
     >
-      <div className="flex flex-col max-w-[1200px]">
+      <div className="flex flex-col max-w-[1200px] mx-auto">
         <div className="p-12 w-full flex-1 flex flex-col justify-start">
           <h2 className="text-2xl font-light mb-2">about brendan</h2>
           <p className="font-light text-slate-700">I have been working in design and development for years. From Printed documents, to virtual grand openings, my experience includes both printed and digital production across a wide range of mediums.</p>
         </div>
-        <div className="p-12 w-fullflex-1 flex flex-row justify-between">
+        <div className="p-12 w-full flex flex-col md:flex-row md:columns-3 gap-12 bg-red justify-between">
           <div className={cardClass}>
           <MdCrop className={iconClass} />
             <h3 className="font-extralight text-center text-5xl text-slate-700 mb-8
